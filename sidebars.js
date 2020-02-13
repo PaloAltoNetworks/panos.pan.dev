e03034fc0d1badb6da2fe2ddfef5ef59e41eb88d
Steven Serrata <sserrata@paloaltonetworks.com>
2020-02-13 12:16:16 -0600

    Reorg sidebars

--- sidebars.js
+++ sidebars.js
@@ -10,41 +10,41 @@ module.exports = {
     {
       type: "category",
       label: "Overview",
-      items: ["panos_api"]
+      items: ["apis/panos_api"]
     },
     {
       type: "category",
       label: "PAN-OS® XML API",
-      items: ["xmlapi_qs"]
+      items: ["apis/xmlapi_qs"]
     },
     {
       type: "category",
       label: "PAN-OS® REST API",
-      items: ["restapi_qs"]
+      items: ["apis/restapi_qs"]
     },
     {
       type: "category",
       label: "PAN Device Framework",
-      items: ["pandevice_qs"]
+      items: ["apis/pandevice_qs"]
     },
     {
       type: "category",
       label: "PAN Python SDK",
-      items: ["panpython_qs", "panpython_apikey", "panpython_op"]
+      items: ["apis/panpython_qs", "apis/panpython_apikey", "apis/panpython_op"]
     },
     {
       type: "category",
       label: "PAN Go SDK",
-      items: ["pango_qs"]
+      items: ["apis/pango_qs"]
     },
     {
       type: "category",
       label: "Tutorials",
       items: [
-        "panos_dag_qs",
-        "panos_tutorials_address_group",
-        "panos_tutorials_rule_hit_count",
-        "panos_tutorials_rulebase_to_csv"
+        "apis/panos_dag_qs",
+        "apis/panos_tutorials_address_group",
+        "apis/panos_tutorials_rule_hit_count",
+        "apis/panos_tutorials_rulebase_to_csv"
       ]
     }
   ],
@@ -52,56 +52,56 @@ module.exports = {
     {
       type: "category",
       label: "Overview",
-      items: ["automation", "terraform_ansible_container"]
+      items: ["automation/automation", "automation/terraform_ansible_container"]
     },
     {
       type: "category",
       label: "Ansible",
-      items: ["ansible_qs"]
+      items: ["automation/ansible_qs"]
     },
     {
       type: "category",
       label: "Terraform",
-      items: ["terraform_qs"]
+      items: ["automation/terraform_qs"]
     },
     {
       type: "category",
       label: "Tutorials",
-      items: ["automation_tutorials_ansible_srule"]
+      items: ["automation/automation_tutorials_ansible_srule"]
     }
   ],
   cloud: [
     {
       type: "category",
       label: "Overview",
-      items: ["cloud"]
+      items: ["cloud/cloud"]
     },
     {
       type: "category",
       label: "AWS",
       items: [
-        "aws_qs",
-        "pan_guard_duty",
-        "autoscaling_aws",
-        "terraform_elb",
-        "aws_alb_sandwich",
-        "xff_demo"
+        "cloud/aws_qs",
+        "cloud/pan_guard_duty",
+        "cloud/autoscaling_aws",
+        "cloud/terraform_elb",
+        "cloud/aws_alb_sandwich",
+        "cloud/xff_demo"
       ]
     },
     {
       type: "category",
       label: "Azure",
-      items: ["azure_qs"]
+      items: ["cloud/azure_qs"]
     },
     {
       type: "category",
       label: "Google",
-      items: ["gcp_qs"]
+      items: ["cloud/gcp_qs"]
     },
     {
       type: "category",
       label: "Private Cloud",
-      items: ["cloudtemplates_qs"]
+      items: ["cloud/cloudtemplates_qs"]
     }
   ]
 };
